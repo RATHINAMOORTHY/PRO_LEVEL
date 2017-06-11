4e29fc5803ecef8cbc5edd04db3a2ef1ae23bec6
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-11 13:37:21 +0530

    Create pro2.java

--- pro2.java
+++ pro2.java
@@ -1,31 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class pro2 {
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
-		ArrayList<Integer> al=new ArrayList<Integer>();
 		System.out.println("ENTER THE VALUE");
-		int n=in.nextInt();
-		String s=String.valueOf(n);
+		String s1=in.nextLine();
 		System.out.println("ENTER THE SIZE TO BE REMOVED");
 		int n1=in.nextInt();
-		int size=s.length()-n1;
-		for(int i=0;i<s.length();i++)
-		{
-			for(int j=i;j<s.length();j++)
-			{
-			if(s.substring(i, j+1).length()==size)
-			{
-				al.add(Integer.valueOf(s.substring(i, j+1)));
-			}
-			}
-		}
-		System.out.print(al.get(0));
-		
-
+		char[] c=s1.toCharArray();
+		Arrays.sort(c);
+		String s3=new String(c);
+		int x=c.length-n1;
+		System.out.print(s3.substring(0,x));
 	}
 
 }
